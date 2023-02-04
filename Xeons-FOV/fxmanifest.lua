19996b6bba48703d292b1a0e08053866b0ab7fd6
asd <86969700+nfinsku@users.noreply.github.com>
2023-02-04 03:01:38 +0200

    Create fxmanifest.lua

---
+++ Xeons-FOV/fxmanifest.lua
@@ -0,0 +1,19 @@
+
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'NLowo'
+description 'Shooting-system-kmrp'
+version '1.0.0'
+
+files {
+    'data/weapons.meta'
+}
+
+client_scripts {
+    'client.lua',
+    'client_two.lua'
+}
+
+
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons.meta'
